c4e3307356300056f06de8e57b82056c67380fcb
arcanist <arcanistzed@gmail.com>
2022-02-20 18:17:32 -0500

    style: formatting
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- scripts/persist.js
+++ scripts/persist.js
@@ -4,107 +4,111 @@ import registerSettings from "./settings.js";
  * @class PersistSheets
  */
 export default class PersistSheets {
-    constructor() {
-        Hooks.on("init", () => {
-            // Register settings
-            registerSettings();
+	constructor() {
+		Hooks.on("init", () => {
+			// Register settings
+			registerSettings();
 
-            // Add the module's API
-            game.modules.get(PersistSheets.ID).api = PersistSheets;
-        });
+			// Add the module's API
+			game.modules.get(PersistSheets.ID).api = PersistSheets;
+		});
 
-        // Register for DevMode
-        Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
-            registerPackageDebugFlag(PersistSheets.ID);
-        });
+		// Register for DevMode
+		Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
+			registerPackageDebugFlag(PersistSheets.ID);
+		});
 
-        // Restore the opened sheets
-        Hooks.on("ready", () => {
-            if (game.settings.get(PersistSheets.ID, "restoreOpened")) this.restoreOpened()
-        });
+		// Restore the opened sheets
+		Hooks.on("ready", () => {
+			if (game.settings.get(PersistSheets.ID, "restoreOpened")) this.restoreOpened();
+		});
 
-        // Restore the sheet positions
-        const restorePosition = app => this.restorePosition(app);
-        Hooks.on("renderDocumentSheet", restorePosition);
-        Hooks.on("renderActorSheet", restorePosition);
-        Hooks.on("renderItemSheet", restorePosition);
-    }
+		// Restore the sheet positions
+		const restorePosition = app => this.restorePosition(app);
+		Hooks.on("renderDocumentSheet", restorePosition);
+		Hooks.on("renderActorSheet", restorePosition);
+		Hooks.on("renderItemSheet", restorePosition);
+	}
 
-    /** The module's ID */
-    static ID = "persist-sheets";
+	/** The module's ID */
+	static ID = "persist-sheets";
 
-    /** DevMode logging helper
-     * @param {Boolean} force - Whether to force logging
-     * @param {*} args - Arguments to log
-     */
-    log(force, ...args) {
-        const shouldLog = force || game.modules.get("_dev-mode")?.api?.getPackageDebugValue(PersistSheets.ID);
-        if (shouldLog) {
-            console.log(PersistSheets.ID, "|", ...args);
-        };
-    };
+	/**
+	 * DevMode logging helper
+	 * @param {boolean} force - Whether to force logging
+	 * @param {*} args - Arguments to log
+	 */
+	log(force, ...args) {
+		const shouldLog = force || game.modules.get("_dev-mode")?.api?.getPackageDebugValue(PersistSheets.ID);
+		if (shouldLog) {
+			console.log(PersistSheets.ID, "|", ...args);
+		}
+	}
 
-    /** Restore the opened sheets */
-    restoreOpened() {
-        Object.entries(game.settings.get(PersistSheets.ID, "sheetStorage"))
-            .forEach(([id, { object, position }]) => {
-                this.log(false, "Restoring opened sheet", id, object, position);
-                if (object) fromUuid(object).then(d => {
-                    d.sheet.render(true, position);
-                });
-            });
-    }
+	/** Restore the opened sheets */
+	restoreOpened() {
+		Object.entries(game.settings.get(PersistSheets.ID, "sheetStorage")).forEach(([id, { object, position }]) => {
+			this.log(false, "Restoring opened sheet", id, object, position);
+			if (object) {
+				fromUuid(object).then(d => {
+					d.sheet.render(true, position);
+				});
+			}
+		});
+	}
 
-    /** Restore the sheet positions
-     * @param {DocumentSheet} sheet
-     */
-    restorePosition(sheet) {
-        Object.entries(game.settings.get(PersistSheets.ID, "sheetStorage"))
-            .forEach(([id, { object, position }]) => {
-                this.log(false, "Restoring sheet position", id, object, position);
-                Object.values(ui.windows)
-                    .filter(w => w.id === id)
-                    .forEach(w => w.setPosition(position));
-            });
+	/**
+	 * Restore the sheet positions
+	 * @param {DocumentSheet} sheet
+	 */
+	restorePosition(sheet) {
+		Object.entries(game.settings.get(PersistSheets.ID, "sheetStorage")).forEach(([id, { object, position }]) => {
+			this.log(false, "Restoring sheet position", id, object, position);
+			Object.values(ui.windows)
+				.filter(w => w.id === id)
+				.forEach(w => w.setPosition(position));
+		});
 
-        const debouncedSave = debounce(() => this.save(sheet), 500);
-        sheet.setPosition = (...args) => {
-            debouncedSave();
-            sheet.constructor.prototype.setPosition.apply(sheet, args);
-        }
-        sheet.close = (...args) => {
-            if (!game.settings.get(PersistSheets.ID, "keepClosedSheets")) this.delete(sheet);
-            sheet.constructor.prototype.close.apply(sheet, args);
-        }
+		const debouncedSave = debounce(() => this.save(sheet), 500);
+		sheet.setPosition = (...args) => {
+			debouncedSave();
+			sheet.constructor.prototype.setPosition.apply(sheet, args);
+		};
+		sheet.close = (...args) => {
+			if (!game.settings.get(PersistSheets.ID, "keepClosedSheets")) this.delete(sheet);
+			sheet.constructor.prototype.close.apply(sheet, args);
+		};
 
-        this.save(sheet);
-    }
+		this.save(sheet);
+	}
 
-    /** Save a sheet's position
-     * @param {DocumentSheet} sheet
-     */
-    save(sheet) {
-        this.log(false, "Saving sheet position", sheet.id, sheet.position);
+	/**
+	 * Save a sheet's position
+	 * @param {DocumentSheet} sheet
+	 */
+	save(sheet) {
+		this.log(false, "Saving sheet position", sheet.id, sheet.position);
 
-        game.settings.set(PersistSheets.ID, "sheetStorage", {
-            ...game.settings.get(PersistSheets.ID, "sheetStorage"),
-            [sheet.id]: {
-                object: sheet.document?.uuid,
-                position: sheet.position
-            },
-        });
-    }
+		game.settings.set(PersistSheets.ID, "sheetStorage", {
+			...game.settings.get(PersistSheets.ID, "sheetStorage"),
+			[sheet.id]: {
+				object: sheet.document?.uuid,
+				position: sheet.position,
+			},
+		});
+	}
 
-    /** Delete a sheet's position
-     * @param {DocumentSheet} sheet
-     */
-    delete(sheet) {
-        this.log(false, "Deleting sheet position", sheet.id);
+	/**
+	 * Delete a sheet's position
+	 * @param {DocumentSheet} sheet
+	 */
+	delete(sheet) {
+		this.log(false, "Deleting sheet position", sheet.id);
 
-        const sheets = game.settings.get(PersistSheets.ID, "sheetStorage");
-        delete sheets[sheet.id];
-        game.settings.set(PersistSheets.ID, "sheetStorage", sheets);
-    }
+		const sheets = game.settings.get(PersistSheets.ID, "sheetStorage");
+		delete sheets[sheet.id];
+		game.settings.set(PersistSheets.ID, "sheetStorage", sheets);
+	}
 }
 
 new PersistSheets();
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,40 +1,45 @@
 import PersistSheets from "./persist.js";
 
+/**
+ * Register the module settings
+ */
 export default function registerSettings() {
-    game.settings.registerMenu(PersistSheets.ID, "reset", {
-        name: game.i18n.localize("persist-sheets.settings.reset.name"),
-        label: game.i18n.localize("persist-sheets.settings.reset.label"),
-        hint: game.i18n.localize("persist-sheets.settings.reset.hint"),
-        icon: "fas fa-eraser",
-        type: class extends FormApplication {
-            constructor(...args) {
-                super(...args);
-                game.settings.set(PersistSheets.ID, "sheetStorage", {});
-                ui.notifications.info(`${PersistSheets.ID} | ${game.i18n.localize("persist-sheets.settings.reset.success")}`);
-            }
-        },
-    });
+	game.settings.registerMenu(PersistSheets.ID, "reset", {
+		name: "persist-sheets.settings.reset.name",
+		label: "persist-sheets.settings.reset.label",
+		hint: "persist-sheets.settings.reset.hint",
+		icon: "fas fa-eraser",
+		type: class extends FormApplication {
+			constructor(...args) {
+				super(...args);
+				game.settings.set(PersistSheets.ID, "sheetStorage", {});
+				ui.notifications.info(
+					`${PersistSheets.ID} | ${game.i18n.localize("persist-sheets.settings.reset.success")}`
+				);
+			}
+		},
+	});
 
-    game.settings.register(PersistSheets.ID, "sheetStorage", {
-        scope: "client",
-        config: false,
-        type: Object,
-    });
+	game.settings.register(PersistSheets.ID, "sheetStorage", {
+		scope: "client",
+		config: false,
+		type: Object,
+	});
 
-    game.settings.register(PersistSheets.ID, "restoreOpened", {
-        name: game.i18n.localize("persist-sheets.settings.restoreOpened.name"),
-        hint: game.i18n.localize("persist-sheets.settings.restoreOpened.hint"),
-        scope: "client",
-        config: true,
-        type: Boolean,
-        default: true,
-    });
-    game.settings.register(PersistSheets.ID, "keepClosedSheets", {
-        name: game.i18n.localize("persist-sheets.settings.keepClosedSheets.name"),
-        hint: game.i18n.localize("persist-sheets.settings.keepClosedSheets.hint"),
-        scope: "client",
-        config: true,
-        type: Boolean,
-        default: false,
-    });
+	game.settings.register(PersistSheets.ID, "restoreOpened", {
+		name: "persist-sheets.settings.restoreOpened.name",
+		hint: "persist-sheets.settings.restoreOpened.hint",
+		scope: "client",
+		config: true,
+		type: Boolean,
+		default: true,
+	});
+	game.settings.register(PersistSheets.ID, "keepClosedSheets", {
+		name: "persist-sheets.settings.keepClosedSheets.name",
+		hint: "persist-sheets.settings.keepClosedSheets.hint",
+		scope: "client",
+		config: true,
+		type: Boolean,
+		default: false,
+	});
 }
